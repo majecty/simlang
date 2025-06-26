8af4ca4c9d406490acbb5fc92105dd4152abb088
juhyung <me@juhyung.dev>
2025-06-26 15:20:17 +0900

    Add S-expression lexer and parser implementation

--- main.go
+++ main.go
@@ -4,4 +4,66 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Go Project!")
+
+	fmt.Println(parse(tokenize("(hello world)")))
+}
+
+type Token struct {
+	Type  string
+	Value string
+}
+
+func tokenize(input string) []Token {
+	tokens := []Token{}
+	var current string
+
+	for i := 0; i < len(input); i++ {
+		ch := input[i]
+
+		switch ch {
+		case '(':
+			tokens = append(tokens, Token{"LPAREN", "("})
+		case ')':
+			if current != "" {
+				tokens = append(tokens, Token{"ATOM", current})
+				current = ""
+			}
+			tokens = append(tokens, Token{"RPAREN", ")"})
+		case ' ', '\n', '\t':
+			if current != "" {
+				tokens = append(tokens, Token{"ATOM", current})
+				current = ""
+			}
+		default:
+			current += string(ch)
+		}
+	}
+
+	return tokens
+}
+
+func parse(tokens []Token) []any {
+	var ast []any
+	var stack [][]any
+	current := []any{}
+
+	for _, token := range tokens {
+		switch token.Type {
+		case "LPAREN":
+			stack = append(stack, current)
+			current = []any{}
+		case "RPAREN":
+			if len(stack) > 0 {
+				last := current
+				current = stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				current = append(current, last)
+			}
+		case "ATOM":
+			current = append(current, token.Value)
+		}
+	}
+
+	ast = current
+	return ast
 }
