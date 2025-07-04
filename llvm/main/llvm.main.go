7ceb9fb237e988e1a13c9da33f2f0131752acd02
juhyung <me@juhyung.dev>
2025-07-05 02:27:05 +0900

    Add simple llvm ir

--- llvm/main/llvm.main.go
+++ llvm/main/llvm.main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+// https://llvm.org/docs/LangRef.html
 func main() {
 	fmt.Println("Hello, Go Project!")
+
+	llvmIR := `; ModuleID = 'simple_module'
+source_filename = "simple_program.ll"
+
+define i32 @main() {
+  ret i32 42
+}`
+
+	filename := "output.ll"
+
+	// Create or open the file for writing
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatalf("Failed to create file %s: %v", filename, err)
+	}
+	defer func() {
+		if closeErr := file.Close(); closeErr != nil {
+			log.Printf("Error closing file %s: %v", filename, closeErr)
+		}
+	}()
+
+	// Write the LLVM IR string to the file
+	_, err = file.WriteString(llvmIR)
+	if err != nil {
+		log.Fatalf("Failed to write LLVM IR to file %s: %v", filename, err)
+	}
+
+	log.Printf("Successfully wrote LLVM IR to %s", filename)
+
 }
