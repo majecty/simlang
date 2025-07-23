e95d9610af20a8bcfe870346d63841f8851cbd3a
juhyung <me@juhyung.dev>
2025-07-23 08:08:20 +0400

    Add llvm add sample

---
+++ llvm/llvmSample2/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// https://llvm.org/docs/LangRef.html
+func main() {
+	fmt.Println("Hello, Go Project!")
+
+	llvmIR := `; ModuleID = 'simple_module'
+source_filename = "simple_program.ll"
+declare i32 @printf(ptr, ...)
+
+@pat = global [14 x i8] c"answer is %d\0A\00"
+
+define i32 @foo() {
+	%1 = add i32 0, 1
+	%2 = add i32 0, 2
+	%3 = add i32 %1, %2
+
+	ret i32 %3
+}
+
+define i32 @main() {
+	%4 = call i32 @foo()
+  %ret = call i32 (ptr, ...) @printf(ptr @pat, i32 %4)
+  ret i32 0
+}`
+
+	filename := "output.ll"
+
+	// Create or open the file for writing
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatalf("Failed to create file %s: %v", filename, err)
+	}
+	defer func() {
+		if closeErr := file.Close(); closeErr != nil {
+			log.Printf("Error closing file %s: %v", filename, closeErr)
+		}
+	}()
+
+	// Write the LLVM IR string to the file
+	_, err = file.WriteString(llvmIR)
+	if err != nil {
+		log.Fatalf("Failed to write LLVM IR to file %s: %v", filename, err)
+	}
+
+	log.Printf("Successfully wrote LLVM IR to %s", filename)
+}
