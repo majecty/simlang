28d7fd059c783b7dbb67296dff3ec4625a984cfd
juhyung <me@juhyung.dev>
2025-08-01 22:29:36 +0900

    Tokenize TCL-like Symbols and Numbers

--- tcllike/lexer/lexer.go
+++ tcllike/lexer/lexer.go
@@ -5,5 +5,53 @@ import "simlang/tcllike/types"
 
 func Tokenize(input string) []types.Token {
 	tokens := []types.Token{}
+	var current string
+
+	for i := 0; i < len(input); i++ {
+		ch := input[i]
+
+		switch ch {
+		case ' ', '\n', '\t':
+			if current != "" {
+				tokens = append(tokens, createToken(current))
+				current = ""
+			}
+		default:
+			current += string(ch)
+		}
+	}
+
+	if current != "" {
+		tokens = append(tokens, createToken(current))
+	}
+
 	return tokens
+}
+
+func createToken(value string) types.Token {
+	if isNumber(value) {
+		return types.Token{Type: types.NUMBER, Value: value}
+	}
+	return types.Token{Type: types.ATOM, Value: value}
+}
+
+func isNumber(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	start := 0
+	if s[0] == '-' || s[0] == '+' {
+		if len(s) == 1 {
+			return false
+		}
+		start = 1
+	}
+
+	for i := start; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			return false
+		}
+	}
+	return true
 }
